09b9aa659fa205a0efbffeef9e92e669105f7a3c
Admirallll <stanislav.yarkeev@gmail.com>
2017-11-30 00:05:16 +0500

    more refactoring

--- async.js
+++ async.js
@@ -4,30 +4,34 @@ exports.isStar = true;
 exports.runParallel = runParallel;
 
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    return new Promise(_resolve => {
+    return new Promise(resolve => {
         let result = [];
         let promisesInProgress = 0;
         let currentIndex = 0;
         update();
 
-        function resolve(index, message) {
+        function handleJobResult(index, message) {
             result[index] = message;
             promisesInProgress--;
             update();
         }
 
+        function startJob(job) {
+            let indexedHandler = handleJobResult.bind(null, currentIndex);
+            new Promise((res, rej) => {
+                setTimeout(() => rej(new Error('Promise timeout')), timeout);
+                job().then(res, rej);
+            }).then(indexedHandler, indexedHandler);
+            promisesInProgress++;
+            currentIndex++;
+        }
+
         function update() {
             while (promisesInProgress < parallelNum && currentIndex < jobs.length) {
-                let job = jobs[currentIndex];
-                new Promise((res, rej) => {
-                    setTimeout(() => rej(new Error('Promise timeout')), timeout);
-                    job().then(res, rej);
-                }).then(resolve.bind(null, currentIndex), resolve.bind(null, currentIndex));
-                promisesInProgress++;
-                currentIndex++;
+                startJob(jobs[currentIndex]);
             }
             if (currentIndex === jobs.length && promisesInProgress === 0) {
-                _resolve(result);
+                resolve(result);
             }
         }
     });
