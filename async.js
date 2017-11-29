1a89905f1904fa0565a60c635802db5fae2e5bf5
Admirallll <stanislav.yarkeev@gmail.com>
2017-11-29 23:47:57 +0500

    some refactoring

--- async.js
+++ async.js
@@ -4,54 +4,31 @@ exports.isStar = true;
 exports.runParallel = runParallel;
 
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    let result = [];
-    let callbacks = [];
-    let promisesInProgress = 0;
-    let currentIndex = 0;
-    let isEnded = false;
-    for (let i = 0; i < jobs.length; i++) {
-        result.push(undefined);
-    }
-    update();
-
-    function resolve(index, message) {
-        result[index] = message;
-        promisesInProgress--;
+    return new Promise(_resolve => {
+        let result = [];
+        let promisesInProgress = 0;
+        let currentIndex = 0;
         update();
-    }
 
-    function reject(index, err) {
-        result[index] = err;
-        promisesInProgress--;
-        update();
-    }
-
-    function update() {
-        while (promisesInProgress < parallelNum && currentIndex < jobs.length) {
-            let job = jobs[currentIndex];
-            new Promise((res, rej) => {
-                setTimeout(() => rej(new Error('Promise timeout')), timeout);
-                job().then(res, rej);
-            }).then(resolve.bind(null, currentIndex), reject.bind(null, currentIndex));
-            promisesInProgress++;
-            currentIndex++;
+        function resolve(index, message) {
+            result[index] = message;
+            promisesInProgress--;
+            update();
         }
-        if (currentIndex === jobs.length && promisesInProgress === 0) {
-            for (let callback of callbacks) {
-                result = callback(result);
+
+        function update() {
+            while (promisesInProgress < parallelNum && currentIndex < jobs.length) {
+                let job = jobs[currentIndex];
+                new Promise((res, rej) => {
+                    setTimeout(() => rej(new Error('Promise timeout')), timeout);
+                    job().then(res, rej);
+                }).then(resolve.bind(null, currentIndex), resolve.bind(null, currentIndex));
+                promisesInProgress++;
+                currentIndex++;
             }
-            isEnded = true;
-        }
-    }
-
-    return {
-        then: function (callback) {
-            if (isEnded) {
-                result = callback(result);
+            if (currentIndex === jobs.length && promisesInProgress === 0) {
+                _resolve(result);
             }
-            callbacks.push(callback);
-
-            return this;
         }
-    };
+    });
 }
